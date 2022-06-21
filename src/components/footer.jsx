a7f1f3cfe2b47beb49ef44722bde49ab0c65084d
Alamin-eng <alaminruben@gmail.com>
2022-06-21 03:58:58 +0100

    footer added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import {
+  faGithub,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Footer({headerBackground}){
+function Footer(){
     return (
-        <div className="p-3 footer-div" style={{
-                backgroundColor:`${headerBackground}`,
-            }}>
-            <p className="text-success">&#9400; 2022 Made With Love  Mohammad Alamin</p>
+        <footer className="footer bg-dark text-white text-center">
+        <div className="footer-icon">
+          <a href="https://github.com/Alamin-eng" target={"blank"}>
+            <FontAwesomeIcon className="fontawe text-white" icon={faGithub} />
+          </a>
+          <a
+            href="mailto:mohammedalaminruben@gmail.com?subject = Feedback&body = Message"
+            target={"blank"}
+          >
+            <FontAwesomeIcon
+              className="fontawe text-warning"
+              icon={faEnvelope}
+            />
+          </a>
+          <a
+            href="mailto:mohammedalaminruben@gmail.com?subject = Feedback&body = Message"
+            target={"blank"}
+          >
+            <FontAwesomeIcon className="fontawe text-info" icon={faFacebook} />
+          </a>
         </div>
+        <p>&copy;2022 Made With Love Mohammad Alamin</p>
+      </footer>
     )
 }
 export default Footer;
